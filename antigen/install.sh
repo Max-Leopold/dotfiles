c9ea492bfa542cb0989fd4fdf171c6aba482a3d8
Max Leopold <max.leopold2111@gmail.com>
2021-12-29 21:59:33 +0100

    Switch antigen for antibody

--- antigen/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-curl -L git.io/antigen > $DOTFILES/antigen/antigen.zsh
--- git/functions.zsh
+++ git/functions.zsh
@@ -1,4 +1,4 @@
 # Pull the latest changes from upstream into your fork
-function remotePull() {
+function git-sync() {
 	git checkout "$1" && git fetch upstream && git rebase upstream/"$1" && git push origin "$1"
 }

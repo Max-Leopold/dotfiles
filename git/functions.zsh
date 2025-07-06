2aeb69dd4db8923df0faa39d686b5cf1970132cc
Max Leopold <max.leopold2111@gmail.com>
2025-07-06 02:20:45 +0200

    Move dotfiles to chezmoi

--- git/functions.zsh
+++
@@ -1,4 +0,0 @@
-# Pull the latest changes from upstream into your fork
-function git-sync() {
-	git checkout "$1" && git fetch upstream && git rebase upstream/"$1" && git push origin "$1"
-}
--- zsh/prompt.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-eval "$(starship init zsh)"
-export STARSHIP_CONFIG="$HOME/.dotfiles/starship/config.toml"
